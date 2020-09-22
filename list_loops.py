fe1bd6c1f41b26df31cde02b44d14027cf5806ca
shahrour <ahmadshahrour951@gmail.com>
2020-09-22 10:45:59 -0700

    Q6

--- list_loops.py
+++ list_loops.py
@@ -13,3 +13,11 @@ songs[-1] = "Billie Jean"
 songs.extend(["Handle With Care", "Shine On You Crazy Diamond",
               "My Beautiful Dark Twisted Fantasy"])
 del songs[4]
+
+# Q6
+animals = ["Cat", "Dog", "Bird"]
+animals.append("Rat")
+print(animals[2])
+del animals[0]
+for animal in animals:
+    print(animal)
